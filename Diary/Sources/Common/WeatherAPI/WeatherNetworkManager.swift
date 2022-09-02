8b9da446e59e4a8b01f75754508beeb407ec3a34
yusw10 <yusw10@naver.com>
2022-09-02 15:35:07 +0900

    feat: CLLocation 으로 위도 경도값 가져옴
    - CLLocation으로 현재 좌표값 구하는 코드 구현

--- Diary/Sources/Common/WeatherAPI/WeatherNetworkManager.swift
+++ Diary/Sources/Common/WeatherAPI/WeatherNetworkManager.swift
@@ -15,7 +15,6 @@ class WeatherNetworkManager {
     
     
     func requestWeatherData(location: Location){
-        //?lat=38.0129678&lon=46.7718772&appid=59df1d9ac01127d4ccba473c6dd62a32
         let urlString = WeatherAPI.weatherURL + "?lat=\(location.latitude)&lon=\(location.longitude)" + "&appid=\(WeatherAPI.boryWeatheAPi)"
         guard let url = URL(string: urlString) else {
             return
@@ -25,13 +24,11 @@ class WeatherNetworkManager {
         request.httpMethod = "GET"
         URLSession.shared.dataTask(with: request) { (data, response, error) in
             guard error == nil else {
-                print("error 1")
                 return
             }
             let successsRange = 200..<300
             guard let statusCode = (response as? HTTPURLResponse)?.statusCode,
                   successsRange.contains(statusCode) else {
-                print("error 2")
                 return
             }
             
@@ -42,11 +39,8 @@ class WeatherNetworkManager {
             
             do {
                 let decodedData = try decoder.decode(WeatherModel.self, from: resultData)
-                print("@@@@@@@")
-                print(decodedData.weather.first?.main)
-                print(decodedData.weather.first?.icon)
             } catch {
-                print("히히")
+                fatalError(error.localizedDescription)
             }
         }.resume()
     }
